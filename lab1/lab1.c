ccc1066919ef3057b951160d4c751ed2e5f96e19
buckeyebrown <brown.4414@osu.edu>
2016-10-19 13:08:10 -0400

    nearly finished with the algo

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -12,9 +12,7 @@
 
 
 int getNumberOfVerticesFromUser();
-//int promptUserForAdjMatrix(int numVertices, int **matrix);
 int promptUserForAdjMatrix(int numVertices, int matrix[12][12]);
-//int** allocateMemoryForAdjMatrix(int numVertices);
 int printMatrix(int numVertices, int matrix[12][12]);
 int initializedMarkedArray(int numVertices, int *marked);
 int depthFirstSearch(int *marked, int matrix[12][12], int i, int numVertices);
@@ -23,26 +21,35 @@ int main(){
 	int matrix[12][12];
 	int numVertices = getNumberOfVerticesFromUser();
 	int marked[numVertices];
-	//int **matrix = allocateMemoryForAdjMatrix(numVertices);
 	promptUserForAdjMatrix(numVertices, matrix);
 	printMatrix(numVertices, matrix);
 	initializedMarkedArray(numVertices, marked);
+	printf("\nComponent: ");
 	depthFirstSearch(marked, matrix, 0, numVertices);
+	printf("\n");
+	printf("Marked vertices: ");
+	int a = 0;
+	while (a < numVertices){
+	printf(" %i", marked[a]);
+	a++;
+	}
+	printf("\n");
 
 return 0;
 }
 
 int depthFirstSearch(int *marked, int matrix[12][12], int i, int numVertices){
 	int j=0;
-	printf("\n%i", i);
+	int num = i + 1;
+	printf(" %i", num);
 	marked[i]=1;
 	while (j < numVertices){
 	if (matrix[i][j] == 1 && !marked[j]){
 		depthFirstSearch(marked, matrix, j, numVertices);
-		j++;
 		}
+	j++;
 	}
-
+	return 0;
 }
 
 int getNumberOfVerticesFromUser(){
@@ -62,19 +69,6 @@ int initializedMarkedArray(int numVertices, int *marked){
  }
  return 0;
 }
-
-/**
-int** allocateMemoryForAdjMatrix(int numVertices){
- int **matrix;
- matrix = (int**) malloc (sizeof(int) * numVertices);
- int i = 0;
- while (i < numVertices){
-	matrix[i] = (int*) malloc (sizeof(int) * numVertices);
-	i++;
- }	
-}
-*/
-//int promptUserForAdjMatrix(int numVertices, int **matrix){
 
 int promptUserForAdjMatrix(int numVertices, int matrix[12][12]){
 	int i = 0;
